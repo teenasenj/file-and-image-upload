658f33f5391f5a35e49d70bb57a723832ab509d4
teenasen <129852390+teenasenj@users.noreply.github.com>
2023-04-27 16:48:29 +0530

    Update Multifileupload.jsx

--- src/file_upload/Multifileupload.jsx
+++ src/file_upload/Multifileupload.jsx
@@ -1,48 +1,54 @@
 
-import React ,{Component}from "react"
-export default class Multifileupload extends Component{
-    fileObj=[];
-    fileArray=[];
-    constructor(props){
-    super(props);
-    this.state={
-    file:[null]
-     }
-    this.uploadMultipleFiles=this.uploadMultipleFiles.bind(this);
-    this.uploadFiles=this.uploadFiles.bind(this);
-    }
-    uploadMultipleFiles(event){
-    this.fileObj.push(event.target.file);
-    for(i=0;i<this.fileObj[0].length;i++){
-        this.fileArray.push(URL.createObjectURL(this.fileObj[0][i]));
-    }
-    this.setState({
-        file:this.fileArray
-    })
-    }
-    uploadFiles(event){
-        event.preventDefault();
-        console.log(this.state.file);
+import React,{Component} from "react";
 
-    }
+export default class MultiFileUpload extends Component{
+         fileObj=[];
+         fileArray=[];
 
-    render(){
-        return( 
-    <div>
-    <form>
-    <div className="form-group multi-preview">
-    {(this.fileArray || []).map(url => (<img src={url} alt=""title="" className="img-thumbnail w-25"/>
-    ))
-    }
-    </div>
-           
-    <div className="form-group"> 
-    <input type="file"  className="form-change" onChange={this.uploadMultipleFiles} 
-    multiple/>
-    <button type="button" className="btn-btn-primary" onClick={this.uploadFiles}>upload</button>
-        </div>
-        </form>
-        </div>
-        )
-    }
+         constructor(props){
+             super(props)
+             this.state={
+             file:[null]
+                       }
+                        this.uploadMultipleFiles=this.uploadMultipleFiles.bind(this);
+                        this.uploadFiles=this.uploadFiles. bind(this);
+                }
+              uploadMultipleFiles(event){
+                  this.fileObj.push(event.target.files);
+                  for(let i=0;i< this.fileObj[0].length;i++){
+                        this.fileArray.push(URL.createObjectURL(this.fileObj[0][i]));
+                  }
+                  this.setState({
+                        file:this.fileArray
+                  })
+              }
+
+              uploadFiles(event){
+                  event.preventDefault();
+                  console.log(this.state.file)
+              }
+
+        render(){
+              return(
+                    <div>
+                    <form>
+                         <div className="form-group multi-preview ">
+                                {/*Upload file preview */}
+                                {(this.fileArray ||[]).map(url=>(
+                                    <img src={url} alt="sample pics" className="img-thumbnail ms-5 w-25"/>
+                                ))}
+                                </div>
+
+                                <div className="form-group ">
+                                <input type="file" className="form-control w-25 mt-5 ms-5" 
+                                onChange={this.uploadMultipleFiles} multiple/>
+
+                                <button type="button" className="btn btn-warning mt-5 ms-5" 
+                                onClick={this.uploadFiles}>Upload</button>
+                          </div>
+                    </form>
+                     </div>
+                )
 }
+}
+
